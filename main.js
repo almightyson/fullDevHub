b7413f0b7206fc0ea554e82de1ca922c2e60e022
pavittar <pavittar1113@gmail.com>
2023-01-30 18:15:34 +0100

    js function on buy button added and new products

---
+++ main.js
@@ -0,0 +1,11 @@
+document.getElementById("a").innerHTML = "20";
+document.getElementById("b").innerHTML = "59";
+document.getElementById("c").innerHTML = "50";
+document.getElementById("d").innerHTML = "36";
+document.getElementById("e").innerHTML = "70";
+document.getElementById("f").innerHTML = "99";
+
+function alertPurchase(){
+    window.alert("item orderd");
+    
+}
